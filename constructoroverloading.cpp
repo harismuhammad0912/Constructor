5be18f65409ce72146919acd2ad570c80b5417ea
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-05-20 17:39:47 +0700

    9

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,14 +13,36 @@ class mahasiswa{
     mahasiswa(int INim, string iNama);
     void cetak();
 };
+
 mahasiswa::mahasiswa(){
 }
 
 mahasiswa::mahasiswa(int INim){
 nim=INim; 
 }
+
 mahasiswa::mahasiswa(string INama){
 nama=INama;
 }
 
+mahasiswa::mahasiswa (int INim, string INama){
+nim=INim;
+nama=INama;
+}
 
+void mahasiswa::cetak(){
+    cout << endl << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("andi");
+    mahasiswa mhs3(010203, "Santi"); 
+    
+    cout << "Mahasiswa 1: "; mhs1.cetak();
+    cout << "Mahasiswa 2: "; mhs2.cetak();
+    cout << "Mahasiswa 3: "; mhs3.cetak();
+
+    return 0;
+}
